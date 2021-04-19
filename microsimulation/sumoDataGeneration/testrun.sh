9bd9b6ed5e702c64847cc8c0be15ab700a703a3c
Helene Sajer <hls327@log-2.nyu.cluster>
2021-04-19 16:26:40 -0400

    edit paths

--- microsimulation/sumoDataGeneration/testrun.sh
+++ microsimulation/sumoDataGeneration/testrun.sh
@@ -1,5 +1,5 @@
-python generateRoutes.py 1 15.0
+~/sumovenv/bin/python generateRoutes.py 1 15.0
 
 /scratch/work/public/singularity/run-sumo-1.9.0.bash netconvert --node-files data/net/my_nodes.nod.xml --edge-files data/net/my_edge.edg.xml -t data/net/my_type.type.xml -o data/net/my_net.net.xml
-/scratch/work/public/singularity/run-sumo-1.9.0.bash sumo -c data/configs/week_0_day_0_config.sumocfg  --save-state.times 86399 --save-state.files data/states/t86399_state.xml --summary-output /scratch/work/public/violetnoise/week_0_day_0_summary.xml
-#/scratch/work/public/singularity/run-sumo-1.9.0.bash sumo -c data/configs/week_0_day_0_config.sumocfg  --save-state.times 86399 --save-state.files data/states/t86399_state.xml --fcd-output /scratch/work/public/violetnoise/week_0_day_0_Trace.xml
+/scratch/work/public/singularity/run-sumo-1.9.0.bash sumo -c data/configs/week_0_day_0_config.sumocfg  --save-state.times 86399 --save-state.files data/states/t86399_state.xml --summary-output /scratch/hls327/week_0_day_0_summary.xml
+#/scratch/work/public/singularity/run-sumo-1.9.0.bash sumo -c data/configs/week_0_day_0_config.sumocfg  --save-state.times 86399 --save-state.files data/states/t86399_state.xml --fcd-output /scratch/hls327/week_0_day_0_Trace.xml
