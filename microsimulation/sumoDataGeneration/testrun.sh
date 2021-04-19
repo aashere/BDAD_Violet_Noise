8c788faa062ee468abf011fa19d27a18bda224ab
Helene Sajer <hsajer3@bloomberg.net>
2021-04-19 15:59:54 -0400

    adjusting bash configs for easy re-run

--- microsimulation/sumoDataGeneration/testrun.sh
+++ microsimulation/sumoDataGeneration/testrun.sh
@@ -1,4 +1,4 @@
-python generateRoutes.py
+python generateRoutes.py 1 15.0
 
 /scratch/work/public/singularity/run-sumo-1.9.0.bash netconvert --node-files data/net/my_nodes.nod.xml --edge-files data/net/my_edge.edg.xml -t data/net/my_type.type.xml -o data/net/my_net.net.xml
-/scratch/work/public/singularity/run-sumo-1.9.0.bash sumo -c data/configs/week_0_day_0_config.sumocfg --fcd-output data/traces/week_0_day_0_Trace.xml --summary-output data/traces/week_0_day_0_summary.xml --save-state.times 86399 --save-state.files data/states/t86399_state.xml
+/scratch/work/public/singularity/run-sumo-1.9.0.bash sumo -c data/configs/week_0_day_0_config.sumocfg  --save-state.times 86399 --save-state.files data/states/t86399_state.xml --summary-output data/traces/week_0_day_0_summary.xml --fcd-output data/traces/week_0_day_0_Trace.xml
