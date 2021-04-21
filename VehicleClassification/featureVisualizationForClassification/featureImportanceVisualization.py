b7e6cc41583ddc25fee27fc749c68bf4ed7db9a7
Kristin <kristink@a-10-27-13-89.dynapool.vpn.nyu.edu>
2021-04-21 17:46:26 -0400

    VehicleClassification code and png

---
+++ VehicleClassification/featureVisualizationForClassification/featureImportanceVisualization.py
@@ -0,0 +1,21 @@
+# visualize feature importance, features: startNodeId, stopNodeId, maxSpeed, hour, minute, turnsCount, consecutiveZerosCount
+
+from matplotlib import pyplot as plt
+import matplotlib.ticker as plticker
+
+plt.bar(x=['startNodeId', 'stopNodeId', 'maxSpeed', 'hour', 'minute', 'turnsCount', 'consecutiveZerosCount'],
+        # the values of height come from the training result
+        height=[], 
+        color=['limegreen', 'dodgerblue', 'crimson', 'purple', 'pink', 'cyan', 'orange'])
+ax = plt.gca()
+ax.set(title='Feature Importance', xlabel='Feature', ylabel='Importance Percentage')
+plt.ylim(0, 1)
+ax.yaxis.set_major_locator(plticker.MultipleLocator(base=0.1)) # set ticks at the y-axis
+ax.yaxis.set_minor_locator(plticker.MultipleLocator(base=0.02)) # set minor ticks at the y-axis
+
+# annotate the bars displaying the value as percentage
+for p in ax.patches:
+    ax.annotate(f'\n{p.get_height()*100:.1f}%',
+      (p.get_x()+p.get_width()/2, p.get_height()), ha='center', va='top', color='white', size=18)
+plt.tight_layout()
+plt.savefig("feature_importance_histogram.png")
---
+++ VehicleClassification/featureVisualizationForClassification/featureVisualizationCarBusComparison.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/Users/kristink/Documents/bigdata/gps/gps_convert_car_type/part-00000-f4fccad9-5e36-4ce0-b968-0c833ce035f8-c000.csv', sep=',')
+
+#normalize data, the percentage of numbers of cars is around 98%
+#use MaxSpeed feature to compare car and bus
+df_max_speed = df.select("id", "type", "maxSpeed").distinct
+car_df = df_max_speed.where(df['type'] == 'Car').sample(frac=0.25)
+bus_df = df_max_speed.where(df['type'] == 'Bus')
+car_max_speed = car_df['maxSpeed']
+bus_max_speed = bus_df['maxSpeed']
+plt.hist([car_max_speed, bus_max_speed])
+plt.xlabel("MaxSpeed")
+plt.ylabel("Frequency")
+plt.title('MaxSpeed Comparison Histogram')
+plt.savefig("max_speed_comparison_histogram.png")
+
+#use TurnsCount feature to compare car and bus
+car_df = df.where(df['type'] == 'Car').sample(frac=0.0204)
+bus_df = df.where(df['type'] == 'Bus')
+car_turns_count = car_df['turnsCount']
+bus_turns_count = bus_df['turnsCount']
+plt.hist([car_turns_count, bus_turns_count])
+plt.xlabel("TurnsCount")
+plt.ylabel("Frequency")
+plt.title('TurnsCount Comparison Histogram')
+plt.savefig("turns_count_comparison_histogram.png")
+
+#use Hour feature to compare car and bus
+bus_df = df.where(df['type'] == 'Bus')
+car_hour = car_df['hour']
+bus_hour = bus_df['hour']
+plt.hist([car_hour, bus_hour])
+plt.xlabel("Hour")
+plt.ylabel("Frequency")
+plt.title('Hour Comparison Histogram')
+plt.savefig("hour_comparison_histogram.png")
+
+#use Minute feature to compare car and bus
+car_df = df.where(df['type'] == 'Car').sample(frac=0.0204)
+bus_df = df.where(df['type'] == 'Bus')
+car_min = car_df['minute']
+bus_min = bus_df['minute']
+plt.hist([car_min, bus_min])
+plt.xlabel("Minute")
+plt.ylabel("Frequency")
+plt.title('Minute Comparison Histogram')
+plt.savefig("minute_comparison_histogram.png")
+
+#use StartNodeId feature to compare car and bus
+car_df = df.where(df['type'] == 'Car').sample(frac=0.0204)
+bus_df = df.where(df['type'] == 'Bus')
+car_start_vertex_id = car_df['start_vertex_id']
+bus_start_vertex_id = bus_df['start_vertex_id']
+plt.hist([car_start_vertex_id, bus_start_vertex_id])
+plt.xlabel("StartNodeId")
+plt.ylabel("Frequency")
+plt.title('StartNodeId Comparison Histogram')
+plt.savefig("start_node_id_comparison_histogram.png")
+
+#use StopNodeId feature to compare car and bus
+car_df = df.where(df['type'] == 'Car').sample(frac=0.0204)
+bus_df = df.where(df['type'] == 'Bus')
+car_stop_vertex_id = car_df['stop_vertex_id']
+bus_stop_vertex_id = bus_df['stop_vertex_id']
+plt.hist([car_start_vertex_id, bus_start_vertex_id])
+plt.xlabel("StopNodeId")
+plt.ylabel("Frequency")
+plt.title('StopNodeId Comparison Histogram')
+plt.savefig("stop_node_id_comparison_histogram.png")
+
+#use ConsecutiveZerosCount feature to compare car and bus
+car_df = df.where(df['type'] == 'Car').sample(frac=0.0204)
+bus_df = df.where(df['type'] == 'Bus')
+car_consecutive_zeros_count = car_df['consecutiveZerosCount']
+bus_consecutive_zeros_count = bus_df['consecutiveZerosCount']
+plt.hist([car_consecutive_zeros_count, bus_consecutive_zeros_count])
+plt.xlabel("ConsecutiveZerosCount")
+plt.ylabel("Frequency")
+plt.title('ConsecutiveZerosCount Comparison Histogram')
+plt.savefig("consecutive_zeros_count_comparison_histogram.png")
