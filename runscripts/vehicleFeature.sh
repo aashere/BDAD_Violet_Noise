643bbe1a74d70af03eb469e50d47404c6eba7af0
Helene Sajer <hsajer3@bloomberg.net>
2021-04-28 17:57:51 -0400

    full data run

--- runscripts/vehicleFeature.sh
+++ runscripts/vehicleFeature.sh
@@ -1,5 +1,5 @@
 spark-submit --master yarn \
 --deploy-mode cluster \
---driver-memory 20G --executor-memory 5G \
---num-executors 20 --executor-cores 5 \
---class VehicleFeatureGen /home/hls327/BDAD_Violet_Noise/VehicleClassification/featurePipeline/vehiclefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed/week_7_day_5_gps /user/jl11257/big_data_project/testing/carbusv1
+--driver-memory 40G --executor-memory 2G \
+--num-executors 40 --executor-cores 5 \
+--class VehicleFeatureGen /home/hls327/BDAD_Violet_Noise/VehicleClassification/featurePipeline/vehiclefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed /user/jl11257/big_data_project/features/vehicleclass
