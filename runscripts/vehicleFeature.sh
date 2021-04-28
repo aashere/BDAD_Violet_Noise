ba6586dcc7b94cf00966e4d880d9618217640382
Helene Sajer <hsajer3@bloomberg.net>
2021-04-28 18:23:17 -0400

    testing file path issue

--- runscripts/vehicleFeature.sh
+++ runscripts/vehicleFeature.sh
@@ -1,8 +1,8 @@
 spark-submit --master yarn \
 --deploy-mode cluster \
---driver-memory 40G --executor-memory 4G \
+--driver-memory 40G --executor-memory 6G \
 --num-executors 60 --executor-cores 5 \
---class VehicleFeatureGen /home/hls327/BDAD_Violet_Noise/VehicleClassification/featurePipeline/vehiclefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed/* /user/jl11257/big_data_project/features/vehicleclass
+--class VehicleFeatureGen /home/hls327/BDAD_Violet_Noise/VehicleClassification/featurePipeline/vehiclefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed/* /user/jl11257/big_data_project/features/classify
 
 hdfs dfs -setfacl -R -m user:hls327:rwx /user/jl11257/big_data_project/features/vehicleclass
 hdfs dfs -setfacl -R -m user:as12366:r-x /user/jl11257/big_data_project/features/vehicleclass
