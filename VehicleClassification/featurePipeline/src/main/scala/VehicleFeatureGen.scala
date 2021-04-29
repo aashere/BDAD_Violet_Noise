eccf8fcfb470de79210d51833ab89d5d2e82ef2a
Helene Sajer <hsajer3@bloomberg.net>
2021-04-28 20:38:57 -0400

    workshop to try to improve performance

--- VehicleClassification/featurePipeline/src/main/scala/VehicleFeatureGen.scala
+++ VehicleClassification/featurePipeline/src/main/scala/VehicleFeatureGen.scala
@@ -21,27 +21,29 @@ object VehicleFeatureGen {
 		val edge_node_df = spark.read.parquet(vertices_path)
 		val gps_df = spark.read.parquet(trace_path).withColumn("edge_id", expr("substring(lane, 0, length(lane)-2)")).repartition(col("id"))
 
-		gps_df.cache()
+		val groupedfts = (gps_df.groupBy("id").agg(min("time"), max("time"), first("type", ignoreNulls=true),max("speed"), avg("speed"))
+								.withColumnRenamed("min(time)","starttime")
+								.withColumnRenamed("max(time)","stoptime")
+								.withColumnRenamed("first(type, true)", "type")
+								.withColumn("type", when($"type" === "Bus", $"type").otherwise(lit("Car")))
+								.withColumnRenamed("max(speed)", "maxSpeed")
+								.withColumnRenamed("avg(speed)", "averageSpeed")
+								.repartition(col("id")))
+		groupedfts.cache()					
 
-		val starttms = gps_df.groupBy("id").agg(min("time")).withColumnRenamed("min(time)","time")
-		val endtms = gps_df.groupBy("id").agg(max("time")).withColumnRenamed("max(time)","time")
-
+		val starttms = groupedfts.select("id","starttime").withColumnRenamed("starttime","time")
 		// start node
 		val start_node_ft = (gps_df.join(starttms, Seq("id","time"), "inner")
 						.join(edge_node_df.select("edge_id", "from_vertex_id"), Seq("edge_id"), "inner")
 						.withColumnRenamed("from_vertex_id","start_vertex_id")
-						.select("id","start_vertex_id"))
+						.select("id","start_vertex_id").repartition(col("id")))
 
+		val endtms = groupedfts.select("id","stoptime").withColumnRenamed("stoptime","time")
 		// end node
 		val stop_node_ft = (gps_df.join(endtms, Seq("id","time"), "inner")
 						.join(edge_node_df.select("edge_id", "to_vertex_id"),  Seq("edge_id"), "inner")
 						.withColumnRenamed("to_vertex_id","stop_vertex_id")
-						.select("id","stop_vertex_id"))
-
-		// calculating speed for trip
-		val speed_fts = (gps_df.groupBy("id").agg(max("speed"), avg("speed"))
-							.withColumnRenamed("max(speed)", "maxSpeed")
-							.withColumnRenamed("avg(speed)", "averageSpeed"))
+						.select("id","stop_vertex_id").repartition(col("id")))
 
 		// calculating the starting hour and minute
 		val time_fts = (starttms.withColumn("hour", ($"time" % (24 * 60 * 60)) / (60 * 60)).withColumn("hour", col("hour").cast("int"))
@@ -54,18 +56,15 @@ object VehicleFeatureGen {
 		val df_angle_minus = (df_valid_angle.withColumn("angleLag", lag("angle", 1) over windowSpec)
 								.withColumn("angleMinus", $"angle" - $"angleLag")
 								.filter(!(isnull($"angleMinus")) && ($"angleMinus" !== 0)))
-		val turncount_ft = df_angle_minus.groupBy("id").count().withColumnRenamed("count", "turnsCount")
+		val turncount_ft = df_angle_minus.groupBy("id").count().withColumnRenamed("count", "turnsCount").repartition(col("id"))
 		//.na.fill(0, Array("turnsCount")).drop("angle")
 
-		val carlabel = (gps_df.groupBy("id").agg(first("type", ignoreNulls=true))
-						.withColumnRenamed("first(type, true)", "type")
-						.withColumn("type", when($"type" === "Bus", $"type").otherwise(lit("Car"))))
-
-		val all_features = (carlabel.join(turncount_ft, Seq("id"),"left").na.fill(0, Array("turnsCount"))
-									.join(time_fts, Seq("id"),"inner")
-									.join(speed_fts, Seq("id"),"inner")
+		val all_features = (groupedfts.join(time_fts, Seq("id"),"inner")
 									.join(start_node_ft, Seq("id"),"inner")
-									.join(stop_node_ft, Seq("id"),"inner"))
+									.join(stop_node_ft, Seq("id"),"inner")
+									.join(turncount_ft, Seq("id"),"left").na.fill(0, Array("turnsCount"))
+									.drop("starttime")
+									.drop("stoptime"))
 
 		all_features.repartition(12).write.mode("append").parquet(write_path)
 	}
