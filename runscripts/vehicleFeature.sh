86df878fe64bbc741b3b709a4b958abd0d1e8102
Helene Sajer <hsajer3@bloomberg.net>
2021-04-28 20:46:49 -0400

    edit runscript

--- runscripts/vehicleFeature.sh
+++ runscripts/vehicleFeature.sh
@@ -3,9 +3,9 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 12G \
 --num-executors 60 --executor-cores 5 \
 spark.yarn.executor.memoryOverhead
---class VehicleFeatureGen /home/hls327/BDAD_Violet_Noise/VehicleClassification/featurePipeline/vehiclefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed/* /user/jl11257/big_data_project/features/vclassify
+--class VehicleFeatureGen /home/hls327/BDAD_Violet_Noise/VehicleClassification/featurePipeline/vehiclefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed/* /user/jl11257/big_data_project/features/vehicleclass
 
-hdfs dfs -setfacl -R -m user:hls327:rwx /user/jl11257/big_data_project/features/vclassify
-hdfs dfs -setfacl -R -m user:as12366:r-x /user/jl11257/big_data_project/features/vclassify
-hdfs dfs -setfacl -R -m user:yl3750:r-x /user/jl11257/big_data_project/features/vclassify
-hdfs dfs -setfacl -R -m group::r-x /user/jl11257/big_data_project/features/vclassify
+hdfs dfs -setfacl -R -m user:hls327:rwx /user/jl11257/big_data_project/features/vehicleclass
+hdfs dfs -setfacl -R -m user:as12366:r-x /user/jl11257/big_data_project/features/vehicleclass
+hdfs dfs -setfacl -R -m user:yl3750:r-x /user/jl11257/big_data_project/features/vehicleclass
+hdfs dfs -setfacl -R -m group::r-x /user/jl11257/big_data_project/features/vehicleclass
