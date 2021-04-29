4e0995c6b4729023908085522bc048df2fa795cf
Helene Sajer <hsajer3@bloomberg.net>
2021-04-28 23:00:39 -0400

    combine runscripts

---
+++ runscripts/featureCalcs.sh
@@ -0,0 +1,21 @@
+spark-submit --master yarn \
+--deploy-mode cluster \
+--driver-memory 40G --executor-memory 12G \
+--num-executors 60 --executor-cores 5 \
+--class VehicleFeatureGen /home/hls327/BDAD_Violet_Noise/VehicleClassification/featurePipeline/vehiclefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed/* /user/jl11257/big_data_project/features/vehicleclass
+
+hdfs dfs -setfacl -R -m user:hls327:rwx /user/jl11257/big_data_project/features/vehicleclass
+hdfs dfs -setfacl -R -m user:as12366:r-x /user/jl11257/big_data_project/features/vehicleclass
+hdfs dfs -setfacl -R -m user:yl3750:r-x /user/jl11257/big_data_project/features/vehicleclass
+hdfs dfs -setfacl -R -m group::r-x /user/jl11257/big_data_project/features/vehicleclass
+
+spark-submit --master yarn \
+--deploy-mode cluster \
+--driver-memory 20G --executor-memory 5G \
+--num-executors 20 --executor-cores 5 \
+--class EdgeFeatureGen /home/hls327/BDAD_Violet_Noise/edgeWeightForecast/featurePipeline/target/scala-2.11/edgefeaturegen_2.11-0.1.jar /user/jl11257/big_data_project/traces/processed/* /user/jl11257/big_data_project/features/edgeregress 900
+
+hdfs dfs -setfacl -R -m user:hls327:rwx /user/jl11257/big_data_project/features/edgeregress
+hdfs dfs -setfacl -R -m user:as12366:r-x /user/jl11257/big_data_project/features/edgeregress
+hdfs dfs -setfacl -R -m user:yl3750:r-x /user/jl11257/big_data_project/features/edgeregress
+hdfs dfs -setfacl -R -m group::r-x /user/jl11257/big_data_project/features/edgeregress
