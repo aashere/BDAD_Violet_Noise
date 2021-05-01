3a00e7f4010b11fc00ee906edde520438f4101fe
Helene Sajer <hsajer3@bloomberg.net>
2021-05-01 11:11:11 -0400

    consolidating kristin and emma's ML code for model exploring, moving old shortest path code

--- VehicleClassification/vehicleClassificationML/ExploratoryModel.scala
+++ VehicleClassification/vehicleClassificationML/ExploratoryModel.scala
@@ -1,15 +1,12 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer, VectorAssembler}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 import java.io._
-import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.LogisticRegression
@@ -17,6 +14,94 @@ import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.expressions.Window
-import org.apache.spark.ml.feature.PCA
-import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+
+
+object VehicleClassification {
+    def main(args: Array[String]) = {
+        val spark = SparkSession.builder().appName("VehicleClassification").getOrCreate
+        import spark.implicits._
+        
+        val seed = 5043
+        val pw = new PrintWriter(new File("result.txt" ))
+        val training_path = "/user/jl11257/big_data_project/features/vehiclesample/training"
+        val oos_test_path = "/user/jl11257/big_data_project/features/vehiclesample/witholdtest"
+
+        val trainSampleData = LoadModelData(spark, training_path)
+        val ooSampleData = LoadModelData(spark, oos_test_path)
+
+        val Array(trainData, testData) = trainSampleData.randomSplit(Array(0.8, 0.2), seed)
+        trainData.cache
+
+        // RANDOM FOREST
+        val randomForestClassifier = (new RandomForestClassifier()
+                                        .setImpurity("gini").setMaxDepth(10).setNumTrees(20)
+                                        .setFeatureSubsetStrategy("auto").setSeed(seed))
+        val randomForestModel = randomForestClassifier.fit(trainData)
+
+        val rfSamplePredictions = randomForestModel.transform(testData)
+        val rfOutSamplePredictions = randomForestModel.transform(ooSampleData)
+
+        // LOGISTIC REGRESSION
+        // train logistic regression model with training data set
+        val logisticRegression = (new LogisticRegression()
+                                    .setMaxIter(100)
+                                    .setRegParam(0.02)
+                                    .setElasticNetParam(0.8))
+        val logisticRegressionModel = logisticRegression.fit(trainData)
+
+        val lrSamplePredictions = logisticRegressionModel.transform(testData)
+        val lrOutSamplePredictions = logisticRegressionModel.transform(ooSampleData)
+
+        trainData.unpersist
+
+        val evaluator = (new BinaryClassificationEvaluator()
+                            .setLabelCol("label")
+                            .setRawPredictionCol("prediction")
+                            .setMetricName("areaUnderPR"))
+
+        pw.write("Random Forest Classifier\n")
+        //println(randomForestModel.toDebugString)
+        pw.write("Random Forest feature importance\n")
+        pw.write(randomForestModel.featureImportances.toString+"\n\n")
+
+        pw.write("Area under Precision-Recall Curve in Down Sampled Data is " + evaluator.evaluate(rfSamplePredictions) + '\n')
+        pw.write("Area under Precision-Recall Curve in Raw Test Data is " + evaluator.evaluate(rfOutSamplePredictions) +"\n\n\n")
+
+        pw.write("Logistic Regression Classifier\n")
+        pw.write(s"Coefficients: ${logisticRegressionModel.coefficients} Intercept: ${logisticRegressionModel.intercept}")
+
+        pw.write("\nArea under Precision-Recall Curve in Down Sampled Data is " + evaluator.evaluate(lrSamplePredictions) + '\n')
+        pw.write("Area under Precision-Recall Curve in Raw Test Data is " + evaluator.evaluate(lrOutSamplePredictions) +"\n\n\n")
+
+        // closing everything
+        pw.close
+        spark.close()
+
+    }
+
+    def LoadModelData(spark:SparkSession, filepath: String): DataFrame = {
+        val df = spark.read.option("mergeSchema", "true").parquet(filepath)
+        val cast_df = df.select(df.columns.map {
+                                    case column@"hour" =>
+                                    col(column).cast("Double").as(column)
+                                    case column@"minute" =>
+                                    col(column).cast("Double").as(column)
+                                    case column@"start_vertex_id" =>
+                                    col(column).cast("Double").as(column)
+                                    case column@"stop_vertex_id" =>
+                                    col(column).cast("Double").as(column)
+                                    case column@"turnsCount" =>
+                                    col(column).cast("Double").as(column)
+                                    case column =>
+                                    col(column)
+                                }: _*)
+        
+        val cols = Array("maxSpeed", "averageSpeed", "hour", "minute", "start_vertex_id", "stop_vertex_id", "turnsCount")
+        val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
+        val featureDf = assembler.transform(df)
+        val indexer = new StringIndexer().setInputCol("type").setOutputCol("label")
+        val labelDf = indexer.fit(featureDf).transform(featureDf)
+
+        return labelDf
+    }
+}
