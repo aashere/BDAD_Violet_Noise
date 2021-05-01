5d2310d86cdf44d4ed2909dc6ba5250a3bbd314a
Helene Sajer <hsajer3@bloomberg.net>
2021-05-01 19:14:30 -0400

    finalize packaging of cv model

--- VehicleClassification/modelTrain/src/main/scala/VehicleClassification.scala
+++ VehicleClassification/modelTrain/src/main/scala/VehicleClassification.scala
@@ -19,64 +19,64 @@ object VehicleClassification {
         import spark.implicits._
         
         val seed = 5043
-        val pw = new PrintWriter(new File("result.txt" ))
+        val pw = new PrintWriter(new File("cv_model_result.txt"))
         val training_path = "/user/jl11257/big_data_project/features/vehiclesample/training"
         val oos_test_path = "/user/jl11257/big_data_project/features/vehiclesample/witholdtest"
 
-        val trainSampleData = LoadModelData(spark, training_path)
-        val ooSampleData = LoadModelData(spark, oos_test_path)
+        val trainSampleData = LoadData(spark, training_path)
+        val ooSampleData = LoadData(spark, oos_test_path)
+        trainSampleData.cache
 
-        val Array(trainData, testData) = trainSampleData.randomSplit(Array(0.8, 0.2), seed)
-        trainData.cache
-
-        // RANDOM FOREST
+        // Begin Pipeline
+        val cols = Array("maxSpeed", "averageSpeed", "turnsCount")
+        val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
+        val indexer = new StringIndexer().setInputCol("type").setOutputCol("label")
         val randomForestClassifier = (new RandomForestClassifier()
-                                        .setImpurity("gini").setMaxDepth(10).setNumTrees(20)
-                                        .setFeatureSubsetStrategy("auto").setSeed(seed))
-        val randomForestModel = randomForestClassifier.fit(trainData)
+                                        .setFeatureSubsetStrategy("auto")
+                                        .setSeed(seed))
+        val stages = Array(assembler, indexer, randomForestClassifier)
+        val pipeline = new Pipeline().setStages(stages)
 
-        val rfSamplePredictions = randomForestModel.transform(testData)
-        val rfOutSamplePredictions = randomForestModel.transform(ooSampleData)
-
-        // LOGISTIC REGRESSION
-        // train logistic regression model with training data set
-        val logisticRegression = (new LogisticRegression()
-                                    .setMaxIter(100)
-                                    .setRegParam(0.02)
-                                    .setElasticNetParam(0.8))
-        val logisticRegressionModel = logisticRegression.fit(trainData)
-
-        val lrSamplePredictions = logisticRegressionModel.transform(testData)
-        val lrOutSamplePredictions = logisticRegressionModel.transform(ooSampleData)
-
-        trainData.unpersist
-
+        // Set Up Training
+        val paramGrid = (new ParamGridBuilder()
+                            .addGrid(randomForestClassifier.maxBins, Array(25, 28, 31))
+                            .addGrid(randomForestClassifier.maxDepth, Array(4, 6, 8))
+                            .addGrid(randomForestClassifier.impurity, Array("entropy", "gini")).build())
         val evaluator = (new BinaryClassificationEvaluator()
                             .setLabelCol("label")
                             .setRawPredictionCol("prediction")
                             .setMetricName("areaUnderPR"))
 
-        pw.write("Random Forest Classifier\n")
-        //println(randomForestModel.toDebugString)
-        pw.write("Random Forest feature importance\n")
-        pw.write(randomForestModel.featureImportances.toString+"\n\n")
+        val cvWithauPRC = (new CrossValidator()
+                            .setEstimator(pipeline)
+                            .setEvaluator(evaluator)
+                            .setEstimatorParamMaps(paramGrid)
+                            .setNumFolds(5))
 
-        pw.write("Area under Precision-Recall Curve in Down Sampled Data is " + evaluator.evaluate(rfSamplePredictions) + '\n')
-        pw.write("Area under Precision-Recall Curve in Raw Test Data is " + evaluator.evaluate(rfOutSamplePredictions) +"\n\n\n")
+        val model = cvWithauPRC.fit(trainSampleData)
 
-        pw.write("Logistic Regression Classifier\n")
-        pw.write(s"Coefficients: ${logisticRegressionModel.coefficients} Intercept: ${logisticRegressionModel.intercept}")
+        trainSampleData.unpersist
 
-        pw.write("\nArea under Precision-Recall Curve in Down Sampled Data is " + evaluator.evaluate(lrSamplePredictions) + '\n')
-        pw.write("Area under Precision-Recall Curve in Raw Test Data is " + evaluator.evaluate(lrOutSamplePredictions) +"\n\n\n")
+        // Evaluate model on test data, both from downsampled train data, and from out of sample testing data
+        val trainSamplePredictions = model.transform(trainSampleData)
+        val outSamplePredictions = model.transform(ooSampleData)
 
-        // closing everything
+        model.write.overwrite().save("/user/jl11257/big_data_project/models/vehicleClassifier/randomForestCVAUPRNoNoise")
+
+        pw.write("Random Forest Classifier with Cross Validation\n")
+        pw.write("Cross Validation Result Model\n")
+        pw.write("Best Model: " + model.bestModel.extractParamMap() + "\n")
+
+        pw.write("Area under Precision-Recall Curve in Down Sampled Data is " + evaluator.evaluate(trainSamplePredictions) + '\n')
+        pw.write("Area under Precision-Recall Curve in Raw Test Data is " + evaluator.evaluate(outSamplePredictions) +"\n")
+
+        pw.write("Model saved under /user/jl11257/big_data_project/models/vehicleClassifier/randomForestCVAUPRNoNoise\n")
+
         pw.close
-        spark.close()
-
+        spark.close
     }
 
-    def LoadModelData(spark:SparkSession, filepath: String): DataFrame = {
+    def LoadData(spark:SparkSession, filepath: String): DataFrame = {
         val df = spark.read.option("mergeSchema", "true").parquet(filepath)
         val cast_df = df.select(df.columns.map {
                                     case column@"turnsCount" =>
@@ -84,75 +84,6 @@ object VehicleClassification {
                                     case column =>
                                     col(column)
                                 }: _*)
-        
-        val cols = Array("maxSpeed", "averageSpeed", "turnsCount")
-        val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
-        val featureDf = assembler.transform(df)
-        val indexer = new StringIndexer().setInputCol("type").setOutputCol("label")
-        val labelDf = indexer.fit(featureDf).transform(featureDf)
-
         return labelDf
     }
-
-
-
-// 3.Add feature column:
-// columns that need to added to feature column
-val cols = Array("maxSpeed", "averageSpeed", "turnsCount")
-
-// VectorAssembler to add feature column
-// input columns - cols
-// feature column - features
-val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
-
-
-// 4.Add label column:
-// StringIndexer define new 'label' column with 'result' column
-val indexer = new StringIndexer().setInputCol("type").setOutputCol("label")
-
-// 5. Build Random Forest model using cross validation to tune parameters
-// split data set pipelineTraining and pipelineTesting, and run on the pipeline 
-// pipelineTraining data set - 70%
-// pipelineTesting data set - 30%
-val seed = 5043
-val Array(pipelineTrainingData, pipelineTestingData) = vehicleInfo.randomSplit(Array(0.7, 0.3), seed)
-val pipelineTrainingDataCache = pipelineTrainingData.cache()
-val pipelineTestingDataCache = pipelineTestingData.cache()
-
-// VectorAssembler and StringIndexer are transformers
-// RandomForestClassifier is the estimator
-val randomForestClassifier = new RandomForestClassifier().setImpurity("gini").setMaxDepth(3).setNumTrees(20).setFeatureSubsetStrategy("auto").setSeed(seed)
-val stages = Array(assembler, indexer, randomForestClassifier)
-
-// build pipeline
-val pipeline = new Pipeline().setStages(stages)
-
-// parameters that needs to tune, we tune
-//  1. max buns
-//  2. max depth
-//  3. impurity
-val paramGrid = new ParamGridBuilder().addGrid(randomForestClassifier.maxBins, Array(25, 28, 31)).addGrid(randomForestClassifier.maxDepth, Array(4, 6, 8)).addGrid(randomForestClassifier.impurity, Array("entropy", "gini")).build()
-
-// evaluator with area under PR
-val evaluatorWithauPRC = new BinaryClassificationEvaluator().setLabelCol("label").setMetricName("areaUnderPR")
-
-// K-Fold cross validation with BinaryClassificationEvaluator of area under PR
-val cvWithauPRC = new CrossValidator().setEstimator(pipeline).setEvaluator(evaluatorWithauPRC).setEstimatorParamMaps(paramGrid).setNumFolds(5).setParallelism(10)
-
-// fit will run cross validation and choose the best set of parameters
-// this will take some time to run
-val cvModelWithauPRC = cvWithauPRC.fit(pipelineTrainingDataCache)
-
-// test cross validated model with test data
-val cvPredictionDfWithauPRC = cvModelWithauPRC.transform(pipelineTestingDataCache)
-cvPredictionDfWithauPRC.show(10)
-
-// 6.Evaluate model: 
-// measure the accuracy of cross validated model with area under PR
-val cvAccuracyWithauPRC = evaluatorWithauPRC.evaluate(cvPredictionDfWithauPRC)
-println(cvAccuracyWithauPRC)
-
-// 7.save model
-cvModelWithauPRC.write.overwrite().save("/user/jl11257/big_data_project/modelSaving/vehicleClassification/cvModelWithauPRC")
-
 }
