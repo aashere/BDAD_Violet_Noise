a3a28715f5d22d5d28506bd2b6ec2e1ed8362f8d
Helene Sajer <hsajer3@bloomberg.net>
2021-05-02 23:59:20 -0400

    typo

--- runscripts/noiseSim.sh
+++ runscripts/noiseSim.sh
@@ -5,7 +5,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_0_* \
 /user/jl11257/big_data_project/traces/noised/week_0 12
 
@@ -14,7 +14,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_1_* \
 /user/jl11257/big_data_project/traces/noised/week_1 12
 
@@ -23,7 +23,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_2_* \
 /user/jl11257/big_data_project/traces/noised/week_2 12
 
@@ -32,7 +32,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_3_* \
 /user/jl11257/big_data_project/traces/noised/week_3 12
 
@@ -41,7 +41,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_4_* \
 /user/jl11257/big_data_project/traces/noised/week_4 12
 
@@ -50,7 +50,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_5_* \
 /user/jl11257/big_data_project/traces/noised/week_5 12
 
@@ -59,7 +59,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_6_* \
 /user/jl11257/big_data_project/traces/noised/week_6 12
 
@@ -68,7 +68,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_7_* \
 /user/jl11257/big_data_project/traces/noised/week_7 12
 
@@ -77,7 +77,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_8_* \
 /user/jl11257/big_data_project/traces/noised/week_8 12
 
@@ -86,7 +86,7 @@ spark-submit --master yarn \
 --driver-memory 40G --executor-memory 16G \
 --num-executors 40 --executor-cores 5 \
 --conf spark.yarn.executor.memoryOverhead=2400 \
---class VehicleFeatureGen /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
+--class NoiseGenerator /home/$(whoami)/BDAD_Violet_Noise/NoiseGenerator/noisegenerator_2.11-0.1.jar \
 /user/jl11257/big_data_project/traces/processed/week_9_* \
 /user/jl11257/big_data_project/traces/noised/week_9 12
 
