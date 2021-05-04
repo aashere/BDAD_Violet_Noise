0084f83b7b33c5801c394858acc7acbd1a7a7445
Helene Sajer <hsajer3@bloomberg.net>
2021-05-03 23:10:10 -0400

    test prediction jar

--- VehicleClassification/modelPredict/src/main/scala/VehiclePrediction.scala
+++ VehicleClassification/modelPredict/src/main/scala/VehiclePrediction.scala
@@ -10,13 +10,15 @@ import org.apache.spark.ml.tuning.CrossValidatorModel
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer, VectorAssembler}
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import scala.collection.mutable.ListBuffer
+
 
 object VehiclePrediction {
     def main(args: Array[String]) = {
         val spark = SparkSession.builder().appName("VehiclePrediction").getOrCreate
 
         val modelPath = "/user/jl11257/big_data_project/models/vehicleClassifier/randomForestFinal"
-        val dataPath = "/user/jl11257/big_data_project/features/vehicleclass/part-00000-f8c4250a-43a1-4003-aa7d-ecf08b452f25-c000.snappy.parquet"
+        val dataPath = "/user/jl11257/big_data_project/features/vehiclesamplenoise/witholdtest"
         //val modelPath = args(0)
         //val dataPath = args(1)
 
@@ -37,11 +39,14 @@ object VehiclePrediction {
         val carCountActual = predictions.filter(col("label") === 0.0).count()
         val carCountPred = predictions.filter(col("prediction") === 0.0).count()
 
-        println("There were " + busCountActual + " buses and " + carCountActual + " cars in the data set provided\n")
-        println("The model predicted " + busCountPred + " buses and " + carCountPred + " cars\n")
+        var result = new ListBuffer[String]()  
 
-        println("Confusion matrix:\n")
-        println(metrics.confusionMatrix)
-        println("\n Area under precision-recall curve = " + evaluator.evaluate(predictions) + "\n")
+        result += "There were " + busCountActual + " buses and " + carCountActual + " cars in the data set provided\n"
+        result += "The model predicted " + busCountPred + " buses and " + carCountPred + " cars\n"
+
+        result += "Confusion matrix:\n" + metrics.confusionMatrix + "\n"
+        result += "Area under precision-recall curve = " + evaluator.evaluate(predictions) + "\n"
+        val outp = spark.sparkContext.parallelize(result)
+        outp.coalesce(1).saveAsTextFile("/user/hls327/predsum")
     }
 }
